374bea05051ffe1ec53e2e06d09197cc3f7866c9
Oscar Anillo <oscarani14@gmail.com>
2023-09-01 16:11:38 -0500

    add initial queries

---
+++ BACKEND/back-app/Server/index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const connectDB = require("./Config/db")
+const app = express();
+require('dotenv').config();
+const { graphqlHTTP } = require("express-graphql");
+const schema = require("./Schema/schema")
+const PORT = process.env.PORT || 5000;
+
+
+connectDB()
+
+app.use("/graphql", graphqlHTTP({
+    schema,
+    graphiql: process.env.NODE_ENV === 'development'
+}))
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+
+
